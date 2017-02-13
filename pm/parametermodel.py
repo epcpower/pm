2e16ebb4132663035cb2cd9f856d744aa335fa6b
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-02-12 20:55:10 -0500

    Add checkbox support

--- pm/parametermodel.py
+++ pm/parametermodel.py
@@ -27,29 +27,35 @@ def to_decimal_or_none(s):
     return result
 
 
+def two_state_checkbox(v):
+    return v in (QtCore.Qt.Checked, True)
+
+
 def ignored_attribute_filter(attribute):
     return not attribute.metadata.get('ignore', False)
 
 
-@epyqlib.utils.general.indexable_attrs(ignore=ignored_attribute_filter,
-                                       convert_on_set=True)
+@epyqlib.utils.general.indexable_attrs(ignore=ignored_attribute_filter)
 @attr.s
 class Parameter(epyqlib.treenode.TreeNode):
     _type = attr.ib(default='parameter', init=False, metadata={'ignore': True})
     name = attr.ib()
     minimum = attr.ib(default=None, convert=to_decimal_or_none)
     maximum = attr.ib(default=None, convert=to_decimal_or_none)
+    factory = attr.ib(default=False, convert=two_state_checkbox)
 
     def __attrs_post_init__(self):
         super().__init__()
 
 
-@epyqlib.utils.general.indexable_attrs(ignore=ignored_attribute_filter,
-                                       convert_on_set=True)
+@epyqlib.utils.general.indexable_attrs(ignore=ignored_attribute_filter)
 @attr.s
 class Group(epyqlib.treenode.TreeNode):
     _type = attr.ib(default='group', init=False, metadata={'ignore': True})
     name = attr.ib()
+    fill0 = epyqlib.utils.general.filler_attribute()
+    fill1 = epyqlib.utils.general.filler_attribute()
+    fill2 = epyqlib.utils.general.filler_attribute()
     children = attr.ib(default=attr.Factory(list), metadata={'ignore': True})
 
     def __attrs_post_init__(self):
@@ -92,14 +98,15 @@ class Encoder(json.JSONEncoder):
         if isinstance(obj, list):
             return obj
 
-        elif isinstance(obj, Parameter):
-            d = attr.asdict(obj, recurse=False, dict_factory=collections.OrderedDict)
+        elif isinstance(obj, (Parameter, Group)):
+            d = attr.asdict(obj,
+                            recurse=False,
+                            dict_factory=collections.OrderedDict,
+                            filter=lambda a, _: a.metadata.get('to_file', True))
 
-        elif isinstance(obj, Group):
-            d = attr.asdict(obj, recurse=False, dict_factory=collections.OrderedDict)
-
-            if obj.tree_parent is None:
-                return [self.default(c) for c in d['children']]
+            if isinstance(obj, Group):
+                if obj.tree_parent is None:
+                    return [self.default(c) for c in d['children']]
 
         elif isinstance(obj, decimal.Decimal):
             i = int(obj)
@@ -118,8 +125,7 @@ class Model(epyqlib.pyqabstractitemmodel.PyQAbstractItemModel):
 
         super().__init__(root=root, attrs=True, parent=parent)
 
-        self.headers = [a.name.title() for a in attr.fields(Parameter)
-                        if not a.metadata.get('ignore', False)]
+        self.headers = [a.name.title() for a in Parameter('').public_fields]
 
     @classmethod
     def from_json_string(cls, s):
@@ -137,11 +143,27 @@ class Model(epyqlib.pyqabstractitemmodel.PyQAbstractItemModel):
     def flags(self, index):
         flags = super().flags(index)
 
-        flags |= QtCore.Qt.ItemIsEditable
+        node = self.node_from_index(index)
+
+        checkable = False
+
+        if isinstance(node, Parameter):
+            if node.public_fields[index.column()].convert is two_state_checkbox:
+                checkable = True
+
+        if checkable:
+            flags |= QtCore.Qt.ItemIsUserCheckable
+        elif node.public_fields[index.column()].metadata.get('editable', True):
+            flags |= QtCore.Qt.ItemIsEditable
 
         return flags
 
     def data_display(self, index):
+        node = self.node_from_index(index)
+
+        if node.public_fields[index.column()].convert is two_state_checkbox:
+            return ''
+
         result = super().data_display(index)
 
         return str(result)
@@ -151,9 +173,21 @@ class Model(epyqlib.pyqabstractitemmodel.PyQAbstractItemModel):
 
         return str(result)
 
+    def data_check_state(self, index):
+        node = self.node_from_index(index)
+
+        if node.public_fields[index.column()].convert is two_state_checkbox:
+            if node[index.column()]:
+                return QtCore.Qt.Checked
+            else:
+                return QtCore.Qt.Unchecked
+
+        return None
+
     def setData(self, index, data, role=None):
+        node = self.node_from_index(index)
+
         if role == QtCore.Qt.EditRole:
-            node = self.node_from_index(index)
             try:
                 node[index.column()] = data
             except ValueError:
@@ -161,5 +195,9 @@ class Model(epyqlib.pyqabstractitemmodel.PyQAbstractItemModel):
             else:
                 self.dataChanged.emit(index, index)
                 return True
+        elif role == QtCore.Qt.CheckStateRole:
+            node[index.column()] = node.public_fields[index.column()].convert(data)
+
+            return True
 
         return False
