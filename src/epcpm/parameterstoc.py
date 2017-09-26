9389da13c33bdbafe940c6912e6357e914e29c0a
Kyle Altendorf <sda@fstab.net>
2017-09-26 09:00:53 -0400

    Move wrapper functions from tests to parameterstoc.py source

--- src/epcpm/parameterstoc.py
+++ src/epcpm/parameterstoc.py
@@ -1,4 +1,7 @@
+import functools
+
 import click
+import pycparser
 
 import epcpm.parametermodel
 
@@ -15,4 +18,116 @@ def _cli(parameters):
         columns=epcpm.parametermodel.columns,
         types=epcpm.parametermodel.types,
     )
+
+    ast = build_ast(model.root)
+
     print()
+
+
+def build_ast(node):
+    def recurse(node, payload):
+        child_items = {}
+        for child in node.children:
+            child_items[child] = recurse(child, payload)
+
+
+def int_literal(value):
+    return pycparser.c_ast.Constant(type='int', value=str(value))
+
+
+Decl = functools.partial(
+    pycparser.c_ast.Decl,
+    name=None,
+    quals=[],
+    storage=[],
+    funcspec=[],
+    init=None,
+    bitsize=None,
+)
+
+
+def Typedef(target, name):
+    return pycparser.c_ast.Typedef(
+        name='',
+        quals=[],
+        storage=['typedef'],
+        type=pycparser.c_ast.TypeDecl(
+            declname=name,
+            quals=[],
+            type=target,
+        ),
+    )
+
+
+def enum(name, enumerators=()):
+    enum_name = '{name}_e'.format(name=name)
+    typedef_name = '{name}_t'.format(name=name)
+
+    enumerators = pycparser.c_ast.EnumeratorList(enumerators=tuple(
+        pycparser.c_ast.Enumerator(name=name, value=int_literal(value))
+        for name, value in enumerators
+    ))
+
+    enumeration = pycparser.c_ast.Enum(
+        name=enum_name,
+        values=enumerators
+    )
+
+    declaration = Decl(type=enumeration)
+
+    typedef = Typedef(
+        target=pycparser.c_ast.Enum(
+            name=enum_name,
+            values=[],
+        ),
+        name=typedef_name,
+    )
+
+    return declaration, typedef
+
+
+def struct(name, members=()):
+    struct_name = f'{name}_s'
+    typedef_name = f'{name}_t'
+
+    struct = pycparser.c_ast.Struct(
+        name=name,
+        decls=tuple(
+            pycparser.c_ast.Decl(
+                name=None,
+                quals=[],
+                storage=[],
+                funcspec=[],
+                type=pycparser.c_ast.TypeDecl(
+                    declname=name,
+                    quals=[],
+                    type=pycparser.c_ast.IdentifierType(
+                        names=(type,),
+                    ),
+                ),
+                init=None,
+                bitsize=bits,
+            )
+            for type, name, bits in members
+        ),
+    )
+
+    decl = pycparser.c_ast.Decl(
+        name=None,
+        quals=[],
+        storage=[],
+        funcspec=[],
+        type=struct,
+        init=None,
+        bitsize=None,
+    )
+
+    typedef = Typedef(
+        target=pycparser.c_ast.Enum(
+            name=struct_name,
+            values=[],
+        ),
+        name=typedef_name,
+    )
+
+    return decl, typedef
--- src/epcpm/tests/test_parameterstoc.py
+++ src/epcpm/tests/test_parameterstoc.py
@@ -61,112 +61,10 @@ def test_pycparser_exploration_parse():
     generator.visit(ast)
 
 
-def int_literal(value):
-    return pycparser.c_ast.Constant(type='int', value=str(value))
-
-
-Decl = functools.partial(
-    pycparser.c_ast.Decl,
-    name=None,
-    quals=[],
-    storage=[],
-    funcspec=[],
-    init=None,
-    bitsize=None,
-)
-
-
-def Typedef(target, name):
-    return pycparser.c_ast.Typedef(
-        name='',
-        quals=[],
-        storage=['typedef'],
-        type=pycparser.c_ast.TypeDecl(
-            declname=name,
-            quals=[],
-            type=target,
-        ),
-    )
-
-
-def enum(name, enumerators=()):
-    enum_name = '{name}_e'.format(name=name)
-    typedef_name = '{name}_t'.format(name=name)
-
-    enumerators = pycparser.c_ast.EnumeratorList(enumerators=tuple(
-        pycparser.c_ast.Enumerator(name=name, value=int_literal(value))
-        for name, value in enumerators
-    ))
-
-    enumeration = pycparser.c_ast.Enum(
-        name=enum_name,
-        values=enumerators
-    )
-
-    declaration = Decl(type=enumeration)
-
-    typedef = Typedef(
-        target=pycparser.c_ast.Enum(
-            name=enum_name,
-            values=[],
-        ),
-        name=typedef_name,
-    )
-
-    return declaration, typedef
-
-
-def struct(name, members=()):
-    struct_name = f'{name}_s'
-    typedef_name = f'{name}_t'
-
-    struct = pycparser.c_ast.Struct(
-        name=name,
-        decls=tuple(
-            pycparser.c_ast.Decl(
-                name=None,
-                quals=[],
-                storage=[],
-                funcspec=[],
-                type=pycparser.c_ast.TypeDecl(
-                    declname=name,
-                    quals=[],
-                    type=pycparser.c_ast.IdentifierType(
-                        names=(type,),
-                    ),
-                ),
-                init=None,
-                bitsize=bits,
-            )
-            for type, name, bits in members
-        ),
-    )
-
-    decl = pycparser.c_ast.Decl(
-        name=None,
-        quals=[],
-        storage=[],
-        funcspec=[],
-        type=struct,
-        init=None,
-        bitsize=None,
-    )
-
-    typedef = Typedef(
-        target=pycparser.c_ast.Enum(
-            name=struct_name,
-            values=[],
-        ),
-        name=typedef_name,
-    )
-
-    return decl, typedef
-
-
 def test_pycparser_exploration_wrapped():
     top_level = []
 
-    top_level.extend(enum(
+    top_level.extend(epcpm.parameterstoc.enum(
         name='enumName',
         enumerators=(
             ('a', 1),
@@ -174,7 +72,7 @@ def test_pycparser_exploration_wrapped():
         ),
     ))
 
-    top_level.extend(struct(
+    top_level.extend(epcpm.parameterstoc.struct(
         name='structName',
         members=(
             ('int16_t', 'a', None),
