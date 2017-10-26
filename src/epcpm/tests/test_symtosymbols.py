fca4cbdbb0b62e1591aabd9f9eddcc46845f757c
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-10-26 09:56:42 -0400

    Test for both roots from load_can_file()

--- src/epcpm/tests/test_symtosymbols.py
+++ src/epcpm/tests/test_symtosymbols.py
@@ -5,8 +5,10 @@ import epcpm.symtoproject
 
 
 def test_load_can_file():
-    root = epcpm.symtoproject.load_can_path(
-        epyqlib.tests.common.symbol_files['customer']
+    parameter_root, symbol_root = epcpm.symtoproject.load_can_path(
+        epyqlib.tests.common.symbol_files['customer'],
+        epyqlib.tests.common.hierarchy_files['customer'],
     )
 
-    assert isinstance(root, epcpm.symbolmodel.Root)
+    assert isinstance(parameter_root, epcpm.parametermodel.Root)
+    assert isinstance(symbol_root, epcpm.symbolmodel.Root)
