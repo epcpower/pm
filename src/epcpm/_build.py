ebea02d823ffa124b48bc614f8ef734f86166245
Kyle Altendorf <kyle.altendorf@epcpower.com>
2017-11-02 12:37:22 -0400

    Try to make working copy clean on AppVeyor

--- src/epcpm/_build.py
+++ src/epcpm/_build.py
@@ -1,9 +1,16 @@
-# This file should be overwritten when a distribution build is done
-
-
-build_system = None
-build_id = None
-build_number = None
-build_version = None
-job_id = None
-job_url = None
+try:
+    from epcpm.__build import (
+        build_system,
+        build_id,
+        build_number,
+        build_version,
+        job_id,
+        job_url,
+    )
+except ImportError:
+    build_system = None
+    build_id = None
+    build_number = None
+    build_version = None
+    job_id = None
+    job_url = None
