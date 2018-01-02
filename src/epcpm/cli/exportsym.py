c966b40f86ce21b2e54b17f97e01d4bc63af58ca
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-01-02 15:05:37 -0500

    Add round-trip test

--- src/epcpm/cli/exportsym.py
+++ src/epcpm/cli/exportsym.py
@@ -2,6 +2,8 @@ import pathlib
 
 import click
 
+import epyqlib.pm.parametermodel
+
 import epcpm.parameterstohierarchy
 import epcpm.project
 import epcpm.cantosym
@@ -26,9 +28,20 @@ def relative_path(target, reference):
 def cli(project_file, sym_file, hierarchy_file):
     project = epcpm.project.load(project_file)
 
+    access_levels, = project.models.parameters.root.nodes_by_filter(
+        filter=(
+            lambda node: isinstance(
+                node,
+                epyqlib.pm.parametermodel.AccessLevels
+            )
+        ),
+    )
+
     sym_builder = epcpm.cantosym.builders.wrap(
         wrapped=project.models.can.root,
+        access_levels=access_levels,
         parameter_uuid_finder=project.models.can.node_from_uuid,
+        parameter_model=project.models.parameters,
     )
 
     hierarchy_builder = epcpm.parameterstohierarchy.builders.wrap(
