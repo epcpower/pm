7650dfedf8a2ef4e8a82a37bb0170e76615b8b30
Kyle Altendorf <kyle.altendorf@epcpower.com>
2018-12-14 15:31:36 -0500

    More test fixup

--- src/epcpm/tests/test_smdxtosunspec.py
+++ src/epcpm/tests/test_smdxtosunspec.py
@@ -25,7 +25,14 @@ def fresh_pathlist():
 
 
 def test_load():
-    project = epcpm.project.loadp(here/'project_with_sunspec'/'project.pmp')
+    project = epcpm.project.loadp(here/'project'/'project.pmp')
+
+    parameter_model = project.models.parameters
+    enumerations = parameter_model.list_selection_roots['enumerations']
+    sunspec_types = epcpm.sunspecmodel.build_sunspec_types_enumeration()
+    enumerations.append_child(sunspec_types)
+    parameter_model.list_selection_roots['sunspec types'] = sunspec_types
+
     requested_models = [1, 17, 103, 65534]
 
     models = epcpm.smdxtosunspec.import_models(
