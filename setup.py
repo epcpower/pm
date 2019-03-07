69fe6b584a09f104e503797fd50701fabec44cd2
Kyle Altendorf <sda@fstab.net>
2019-03-07 11:11:13 -0500

    epyqlib==2019.3.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     install_requires=[
         'canmatrix',
         'click',
-        'epyqlib>=2019.2.2',
+        'epyqlib>=2019.3.1',
         'graham',
         'lxml',
         'openpyxl',
