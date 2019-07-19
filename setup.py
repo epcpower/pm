bf6316a7d825576d4f8ed867e4e64d45e0f9472b
Kyle Altendorf <sda@fstab.net>
2019-07-19 13:33:41 -0700

    update epyqlib

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     install_requires=[
         'canmatrix',
         'click',
-        'epyqlib>=2019.5.1',
+        'epyqlib>=2019.7',
         'graham',
         'lxml',
         'openpyxl',
