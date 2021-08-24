4c596dc93987aeb8c9640f5a6310418ff96d8c1e
Gordon Schmidt <gordon.schmidt@epcpower.com>
2021-08-24 11:35:09 -0700

    feat (SC-201): use graham for static modbus script json output, add address to static modbus

--- src/epcpm/cli/sunspectostaticmodbus.py
+++ src/epcpm/cli/sunspectostaticmodbus.py
@@ -1,7 +1,7 @@
 import click
 import csv
 import distutils.util
-import json
+import graham
 import uuid
 
 import epcpm.sunspecmodel
@@ -28,56 +28,80 @@ def map_type_uuid(input, sunspec_types, staticmodbus_types):
     raise ValueError("Error: failure to map type")
 
 
-def create_function_data(
+def create_data_object(
     input_sunspec_data, scale_factor_uuid_map, sunspec_types, staticmodbus_types
 ):
-    function_data = dict()
-    # TODO: This will likely be be split up into a separate method when bitfields and tables are implemented.
-    if input_sunspec_data["class_name"] == "DataPoint":
-        function_data["_type"] = "function_data"
-    elif input_sunspec_data["class_name"] == "DataPointBitfield":
-        function_data["_type"] = "function_data_bitfield"
+    # Determine the type of data object to create and then create the object and return it.
+    if input_sunspec_data["class_name"] == epcpm.sunspecmodel.DataPoint.__name__:
+        return create_function_data(input_sunspec_data, scale_factor_uuid_map, sunspec_types, staticmodbus_types)
+    elif input_sunspec_data["class_name"] == epcpm.sunspecmodel.DataPointBitfield.__name__:
+        return create_function_data_bitfield(input_sunspec_data, sunspec_types, staticmodbus_types)
     else:
-        function_data[
-            "_type"
-        ] = f"Unsupported class_name '{input_sunspec_data['class_name']}' in create_function_data."
+        raise ValueError(f"Unsupported class_name '{input_sunspec_data['class_name']}' in create_data")
+
+
+def create_function_data(input_sunspec_data, scale_factor_uuid_map, sunspec_types, staticmodbus_types):
+    function_data = epcpm.staticmodbusmodel.FunctionData()
+
     if input_sunspec_data["scale_factor_uuid"]:
-        function_data["factor_uuid"] = scale_factor_uuid_map[
+        function_data.factor_uuid = scale_factor_uuid_map[
             input_sunspec_data["scale_factor_uuid"]
         ]
     else:
-        function_data["factor_uuid"] = None
-    function_data["parameter_uuid"] = (
+        function_data.factor_uuid = None
+    function_data.parameter_uuid = (
         input_sunspec_data["parameter_uuid"]
         if input_sunspec_data["parameter_uuid"]
         else None
     )
-    function_data["not_implemented"] = bool(
+    function_data.not_implemented = bool(
         distutils.util.strtobool(input_sunspec_data["not_implemented"])
     )
     if input_sunspec_data["type_uuid"]:
-        function_data["type_uuid"] = map_type_uuid(
+        function_data.type_uuid = map_type_uuid(
             input_sunspec_data["type_uuid"], sunspec_types, staticmodbus_types
         )
     else:
-        function_data["type_uuid"] = None
-    function_data["size"] = int(input_sunspec_data["size"])
-    function_data["enumeration_uuid"] = (
+        function_data.type_uuid = None
+    function_data.size = int(input_sunspec_data["size"])
+    function_data.enumeration_uuid = (
         input_sunspec_data["enumeration_uuid"]
         if input_sunspec_data["enumeration_uuid"]
         else None
     )
-    function_data["units"] = (
+    function_data.units = (
         input_sunspec_data["units"] if input_sunspec_data["units"] else None
     )
     if input_sunspec_data["uuid"] in scale_factor_uuid_map:
         # This is a scale factor UUID, so set UUID to the newly generated UUID value.
-        function_data["uuid"] = scale_factor_uuid_map[input_sunspec_data["uuid"]]
+        function_data.uuid = scale_factor_uuid_map[input_sunspec_data["uuid"]]
     else:
-        function_data["uuid"] = generate_uuid()
-    function_data["address"] = int(input_sunspec_data["modbus_address"])
+        function_data.uuid = generate_uuid()
+    function_data.address = int(input_sunspec_data["modbus_address"])
 
     return function_data
+
+
+def create_function_data_bitfield(input_sunspec_data, sunspec_types, staticmodbus_types):
+    function_data_bitfield = epcpm.staticmodbusmodel.FunctionDataBitfield()
+
+    function_data_bitfield.parameter_uuid = (
+        input_sunspec_data["parameter_uuid"]
+        if input_sunspec_data["parameter_uuid"]
+        else None
+    )
+    if input_sunspec_data["type_uuid"]:
+        function_data_bitfield.type_uuid = map_type_uuid(
+            input_sunspec_data["type_uuid"], sunspec_types, staticmodbus_types
+        )
+    else:
+        function_data_bitfield.type_uuid = None
+    function_data_bitfield.size = int(input_sunspec_data["size"])
+    # A bitfield would never be a scale factor, so no need to check for uuid in scale_factor_uuid_map.
+    function_data_bitfield.uuid = generate_uuid()
+    function_data_bitfield.address = int(input_sunspec_data["modbus_address"])
+
+    return function_data_bitfield
 
 
 def generate_uuid_mapping_for_scale_factor(input_sunspec_data):
@@ -103,12 +127,6 @@ def cli(input_sunspec_filename, output_staticmodbus_filename):
     """SunSpec CSV file to static modbus JSON file"""
     sunspec_types = epcpm.sunspecmodel.build_sunspec_types_enumeration()
     staticmodbus_types = epcpm.staticmodbusmodel.build_staticmodbus_types_enumeration()
-    data = {
-        "_type": "root",
-        "name": "Static Modbus",
-        "children": [],
-        "uuid": generate_uuid(),
-    }
 
     with open(input_sunspec_filename, "r", newline="") as csv_file:
         csv_reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC)
@@ -136,15 +154,20 @@ def cli(input_sunspec_filename, output_staticmodbus_filename):
             input_sunspec_csv
         )
 
+        # Create root object to store the static modbus model.
+        root = epcpm.staticmodbusmodel.Root()
+
         # Transform input data from sunspec to staticmodbus output.
         for data_row in input_sunspec_csv:
-            root_child = create_function_data(
+            root_child = create_data_object(
                 data_row, scale_factor_uuid_map, sunspec_types, staticmodbus_types
             )
-            data["children"].append(root_child)
+            root.children.append(root_child)
 
-        # Output staticmodbus JSON file.
-        with open(
-            output_staticmodbus_filename, "w", encoding="utf-8"
-        ) as output_staticmodbus_fp:
-            json.dump(data, output_staticmodbus_fp, ensure_ascii=False, indent=4)
+        # Output staticmodbus JSON file using graham.
+        with open(output_staticmodbus_filename, "w", newline="\n") as f:
+            staticmodbus_out = graham.dumps(root, indent=4).data
+            f.write(staticmodbus_out)
+
+            if not staticmodbus_out.endswith("\n"):
+                f.write("\n")
