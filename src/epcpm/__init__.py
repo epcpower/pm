0a9e3f0da9a2965d5dd8257484dc8a668c5d5999
Alex Anker <alex.anker@epcpower.com>
2022-01-26 14:26:20 -0800

    black

--- src/epcpm/__init__.py
+++ src/epcpm/__init__.py
@@ -5,6 +5,7 @@ import subprocess
 def get_git_revision_hash() -> str:
     return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 
+
 __version__ = "0.0.0"
 __sha__ = get_git_revision_hash()
 # del get_versions
