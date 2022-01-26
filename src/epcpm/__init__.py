483fc3ecb53da161c7903a95ce173820b9e38199
Alex Anker <alex.anker@epcpower.com>
2022-01-26 13:12:36 -0800

    black

--- src/epcpm/__init__.py
+++ src/epcpm/__init__.py
@@ -1,18 +1,18 @@
 import epcpm._build
 
-#from ._version import get_versions
+# from ._version import get_versions
 
-#__version__ = get_versions()["version"]
-#__sha__ = get_versions()["full-revisionid"]
-#del get_versions
+# __version__ = get_versions()["version"]
+# __sha__ = get_versions()["full-revisionid"]
+# del get_versions
 
-#__version_tag__ = "v{}".format(__version__)
-#__build_tag__ = epcpm._build.job_id
+# __version_tag__ = "v{}".format(__version__)
+# __build_tag__ = epcpm._build.job_id
 
 
 __version__ = "1.2.3"
 __sha__ = "4.5.6"
-#del get_versions
+# del get_versions
 
 __version_tag__ = "v{}".format(__version__)
 __build_tag__ = epcpm._build.job_id
