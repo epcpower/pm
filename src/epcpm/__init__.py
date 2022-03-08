76a89f1d79e6f6513934166f7a340c85bca74f6c
Gordon Schmidt <gordon.schmidt@epcpower.com>
2022-03-08 10:30:53 -0800

    feat (SC-401): add comment for versioning (real)

--- src/epcpm/__init__.py
+++ src/epcpm/__init__.py
@@ -6,6 +6,8 @@ def get_git_revision_hash() -> str:
     return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 
 
+# For development and git commit, the __version__ is set to the build placeholder 0.0.0
+# For release, the __version__ is modified by poetry dynamic versioning with the actual released version
 __version__ = "0.0.0"
 __sha__ = get_git_revision_hash()
 
