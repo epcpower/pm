baa84d3393dbf585ba648221ca4d56937964403f
alex anker <alex.anker@epcpower.com>
2022-04-12 15:11:23 -0700

    update comment 2

--- src/epcpm/__init__.py
+++ src/epcpm/__init__.py
@@ -6,8 +6,10 @@ def get_git_revision_hash() -> str:
     return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 
 
-# For development and git commit, the __version__ is set to the build placeholder 0.0.0
-# For release, the __version__ is modified by poetry dynamic versioning with the GitHub tagged version
+# For development and git commit, the __version__ variable below is set
+# to the build placeholder "0.0.0". (In other words: leave alone!)
+# For release/distribution, the __version__ variable below is modified
+# during CI by poetry dynamic versioning with the github tagged version.
 __version__ = "0.0.0"
 __sha__ = get_git_revision_hash()
 
