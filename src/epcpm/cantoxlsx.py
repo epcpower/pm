6c1688037b416b9a52a8535b6c3f58c703eec6be
Gordon Schmidt <gordon.schmidt@epcpower.com>
2024-08-14 11:55:06 -0700

    feat(SC-2175):more selective filter of Before/After nodes

--- src/epcpm/cantoxlsx.py
+++ src/epcpm/cantoxlsx.py
@@ -33,7 +33,7 @@ FILTER_GROUPS = [
     "B. Other -> Authorization",
     "B. Other -> Debug",
 ]
-FILTER_NODES = ["Before", "After"]
+FILTER_NODES = [f"Tables{PATH_SEPARATOR}Before", f"Tables{PATH_SEPARATOR}After"]
 CELL_SIDE = openpyxl.styles.Side(border_style="thin", color="000000")
 CELL_BORDER = openpyxl.styles.Border(
     top=CELL_SIDE, left=CELL_SIDE, right=CELL_SIDE, bottom=CELL_SIDE
